17c875b223e9fc549fe44cb097b978f8d753b35f
Itisha Chouhan <itisharajput00@gmail.com>
2026-02-24 13:35:19 +0530

    Added inline comments and finally block to Task1
    Added comments to explain file handling logic and included a finally block to demonstrates cleanup behavior.

--- Task1.py
+++ Task1.py
@@ -1,8 +1,14 @@
+# Task 1: Demonstrates file reading using with statement and exception handling
+
+# Try block to handle file operations safely
 try:
+# Opening the file in read mode using with statement
     with open('sample.txt', 'r') as fh:
         for line in fh:
             print(line.strip())
             # print(line , end="")
-       
+# Haandling the case where the file does not exist       
 except FileNotFoundError:
     print("The file 'sample.txt' was not found.") 
+finally:
+    print("File operation completed.")
